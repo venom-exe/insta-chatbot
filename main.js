68cfc9db5732604d8753b3bb1d3227b52f39f93e
venom-exe <87944936+venom-exe@users.noreply.github.com>
2021-09-14 08:45:45 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const Insta = require('@androz2091/insta.js');
+const client = new Insta.Client();
+const chatbot = require("node-fetch").default;
+
+client.on('connected', () => {
+    console.log(`${client.user.username} Is Ready Now For Chats`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.id === client.user.id) return
+    message.markSeen();
+
+    if(message.content.toLowerCase().includes('hi')){ 
+        return message.chat.sendMessage('VENOM IS MY DEVELOPER CHE OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
+    } else
+    chatbot(`https://kukichatai.vercel.app/message=${encodeURIComponent(message.content)}`)
+    .then(res => res.json())
+    .then(json => {
+      message.chat.sendMessage(json.reply);
+    }).catch(err => {});
+});
+
+client.login('<instagram-username>', '<instagram-password>');
