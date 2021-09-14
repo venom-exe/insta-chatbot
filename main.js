b57b60f40223ff2269bc01b2609db72e16982f1b
venom-exe <87944936+venom-exe@users.noreply.github.com>
2021-09-14 09:05:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ client.on('messageCreate', (message) => {
     message.markSeen();
 
     if(message.content.toLowerCase().includes('hi')){ 
-        return message.chat.sendMessage('VENOM IS MY DEVELOPER CHE OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
+        return message.chat.sendMessage('VENOM IS MY DEVELOPER CHECK OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
     } else
     chatbot(`https://kukichatai.vercel.app/message=${encodeURIComponent(message.content)}`)
     .then(res => res.json())
